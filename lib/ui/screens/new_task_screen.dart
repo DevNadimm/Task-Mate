11e8643fa3493b59ee175d12dd6bd7cd89ad5721
DevNadimm <nadimchowdhury87@gmail.com>
2024-10-09 20:13:00 +0600

    update

--- lib/ui/screens/new_task_screen.dart
+++ lib/ui/screens/new_task_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:task_mate/ui/screens/add_new_task_screen.dart';
+import 'package:task_mate/ui/widgets/image_background.dart';
 
 class NewTaskScreen extends StatelessWidget {
   const NewTaskScreen({super.key});
@@ -7,14 +8,14 @@ class NewTaskScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Scaffold(
-        body: const Center(
+      body: const ImageBackground(
+        child: Center(
           child: Text('New Task'),
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => _onTapBottomNavBar(context),
-          child: const Icon(Icons.add_circle_rounded),
-        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _onTapBottomNavBar(context),
+        child: const Icon(Icons.add_circle_rounded),
       ),
     );
   }
