fa23fff8e63c1d44dafcb1466f0393843783976f
DevNadimm <nadimchowdhury87@gmail.com>
2024-10-10 22:32:57 +0600

    update

---
+++ lib/data/services/network_caller.dart
@@ -0,0 +1,68 @@
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart';
+import 'package:task_mate/data/models/network_response.dart';
+import 'package:http/http.dart' as http;
+
+class NetworkCaller {
+  static Future<NetworkResponse> getRequest(String url) async {
+    try {
+      final uri = Uri.parse(url);
+      final response = await http.get(uri);
+      printResponse(url, response);
+      if (response.statusCode == 200) {
+        final decodeData = jsonDecode(response.body);
+        return NetworkResponse(
+            statusCode: response.statusCode,
+            isSuccess: true,
+            responseData: decodeData);
+      } else {
+        return NetworkResponse(
+          statusCode: response.statusCode,
+          isSuccess: false,
+        );
+      }
+    } catch (error) {
+      return NetworkResponse(
+        statusCode: -1,
+        isSuccess: false,
+        errorMessage: error.toString(),
+      );
+    }
+  }
+
+  static Future<NetworkResponse> postRequest(
+      String url, Map<String, dynamic>? body) async {
+    try {
+      final uri = Uri.parse(url);
+      final response = await http.post(uri,
+          body: jsonEncode(body),
+          headers: {'Content-Type': 'application/json'});
+      printResponse(url, response);
+      if (response.statusCode == 200) {
+        final decodeData = jsonDecode(response.body);
+        return NetworkResponse(
+            statusCode: response.statusCode,
+            isSuccess: true,
+            responseData: decodeData);
+      } else {
+        return NetworkResponse(
+          statusCode: response.statusCode,
+          isSuccess: false,
+        );
+      }
+    } catch (error) {
+      return NetworkResponse(
+        statusCode: -1,
+        isSuccess: false,
+        errorMessage: error.toString(),
+      );
+    }
+  }
+
+  static void printResponse(String url, Response response) {
+    debugPrint(
+      'URL: $url\nRESPONSE CODE: ${response.statusCode}\nBODY: ${response.body}',
+    );
+  }
+}
