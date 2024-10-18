bc74148b4ca35244cb066604b62a060b4bd5081c
DevNadimm <nadimchowdhury87@gmail.com>
2024-10-18 11:42:28 +0600

    update

--- lib/features/tasks/screens/progress_task_screen.dart
+++ lib/features/tasks/screens/progress_task_screen.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:task_mate/core/network/network_caller.dart';
+import 'package:task_mate/core/network/network_response.dart';
+import 'package:task_mate/core/utils/toast_message.dart';
+import 'package:task_mate/core/utils/urls.dart';
 import 'package:task_mate/features/tasks/widgets/task_card.dart';
+import 'package:task_mate/models/task_list_model.dart';
+import 'package:task_mate/models/task_model.dart';
 
-class ProgressTaskScreen extends StatelessWidget {
+class ProgressTaskScreen extends StatefulWidget {
   const ProgressTaskScreen({super.key});
+
+  @override
+  State<ProgressTaskScreen> createState() => _ProgressTaskScreenState();
+}
+
+class _ProgressTaskScreenState extends State<ProgressTaskScreen> {
+  bool inProgressTaskList = false;
+  List<TaskModel> progressTaskList = [];
+
+  @override
+  void initState() {
+    _getProgressTaskList();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,21 +32,46 @@ class ProgressTaskScreen extends StatelessWidget {
         child: Column(
           children: [
             const SizedBox(height: 16),
-            ListView.separated(
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              itemCount: 10,
-              itemBuilder: (context, index) {
-                // return TaskCard();
-              },
-              separatorBuilder: (context, index) {
-                return const SizedBox(height: 8);
-              },
+            Visibility(
+              visible: !inProgressTaskList,
+              replacement: const Center(
+                child: CircularProgressIndicator(),
+              ),
+              child: ListView.separated(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: progressTaskList.length,
+                itemBuilder: (context, index) {
+                  return TaskCard(
+                    task: progressTaskList[index],
+                  );
+                },
+                separatorBuilder: (context, index) {
+                  return const SizedBox(height: 8);
+                },
+              ),
             ),
             const SizedBox(height: 16),
           ],
         ),
       ),
     );
+  }
+
+  Future _getProgressTaskList() async {
+    progressTaskList.clear();
+    setState(() => inProgressTaskList = true);
+
+    NetworkResponse networkResponse =
+        await NetworkCaller.getRequest(Urls.getProgressTask);
+
+    if (networkResponse.isSuccess) {
+      final taskListModel =
+          TaskListModel.fromJson(networkResponse.responseData);
+      progressTaskList = taskListModel.taskList ?? [];
+    } else {
+      ToastMessage.errorToast(networkResponse.errorMessage);
+    }
+    setState(() => inProgressTaskList = false);
   }
 }
