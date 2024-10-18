b0c36c3c8091f4b693a35759bae5c747d9e271c8
DevNadimm <nadimchowdhury87@gmail.com>
2024-10-18 11:32:02 +0600

    update

--- lib/features/tasks/screens/cancelled_task_screen.dart
+++ lib/features/tasks/screens/cancelled_task_screen.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:task_mate/core/network/network_caller.dart';
+import 'package:task_mate/core/network/network_response.dart';
+import 'package:task_mate/core/utils/toast_message.dart';
+import 'package:task_mate/core/utils/urls.dart';
 import 'package:task_mate/features/tasks/widgets/task_card.dart';
+import 'package:task_mate/models/task_list_model.dart';
+import 'package:task_mate/models/task_model.dart';
 
-class CancelledTaskScreen extends StatelessWidget {
+class CancelledTaskScreen extends StatefulWidget {
   const CancelledTaskScreen({super.key});
+
+  @override
+  State<CancelledTaskScreen> createState() => _CancelledTaskScreenState();
+}
+
+class _CancelledTaskScreenState extends State<CancelledTaskScreen> {
+  bool inProgressTaskList = false;
+  List<TaskModel> cancelledTaskList = [];
+
+  @override
+  void initState() {
+    _getCancelledTaskList();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,23 +32,44 @@ class CancelledTaskScreen extends StatelessWidget {
         child: Column(
           children: [
             const SizedBox(height: 16),
-            ListView.separated(
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              itemCount: 10,
-              itemBuilder: (context, index) {
-                // return TaskCard(
-                //   task: SizedBox(),
-                // );
-              },
-              separatorBuilder: (context, index) {
-                return const SizedBox(height: 8);
-              },
+            Visibility(
+              visible: !inProgressTaskList,
+              replacement: const Center(
+                child: CircularProgressIndicator(),
+              ),
+              child: ListView.separated(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: cancelledTaskList.length,
+                itemBuilder: (context, index) {
+                  return TaskCard(task: cancelledTaskList[index]);
+                },
+                separatorBuilder: (context, index) {
+                  return const SizedBox(height: 8);
+                },
+              ),
             ),
             const SizedBox(height: 16),
           ],
         ),
       ),
     );
+  }
+
+  Future _getCancelledTaskList() async {
+    cancelledTaskList.clear();
+    setState(() => inProgressTaskList = true);
+
+    NetworkResponse networkResponse =
+        await NetworkCaller.getRequest(Urls.getCancelledTask);
+
+    if (networkResponse.isSuccess) {
+      final taskListModel =
+          TaskListModel.fromJson(networkResponse.responseData);
+      cancelledTaskList = taskListModel.taskList ?? [];
+    } else {
+      ToastMessage.errorToast(networkResponse.errorMessage);
+    }
+    setState(() => inProgressTaskList = false);
   }
 }
