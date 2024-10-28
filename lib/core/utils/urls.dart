ae80e55f1b61fed3ef124b209e1f86468fa383c8
DevNadimm <nadimchowdhury87@gmail.com>
2024-10-28 13:02:16 +0600

    update

--- lib/core/utils/urls.dart
+++ lib/core/utils/urls.dart
@@ -1,5 +1,7 @@
 class Urls {
-  static String baseUrl = 'http://152.42.163.176:2006/api/v1';
+  Urls._();
+
+  static String baseUrl = 'http://35.73.30.144:2005/api/v1';
   static String registration = '$baseUrl/Registration';
   static String login = '$baseUrl/Login';
   static String recoverVerifyEmail = '$baseUrl/RecoverVerifyEmail/';
