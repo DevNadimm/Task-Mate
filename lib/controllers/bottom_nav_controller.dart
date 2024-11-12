a77621123fcc23961a160b097b6fe8b1c1ffd8a1
DevNadimm <nadimchowdhury87@gmail.com>
2024-11-12 09:20:34 +0600

    update

--- lib/controllers/bottom_nav_controller.dart
+++ lib/controllers/bottom_nav_controller.dart
@@ -3,10 +3,11 @@ import 'package:get/get.dart';
 class BottomNavController extends GetxController {
   static BottomNavController instance = Get.find<BottomNavController>();
 
-  int currentIndex = 0;
+  int _currentIndex = 0;
+  int get currentIndex => _currentIndex;
 
   void changeIndex(int newIndex) {
-    currentIndex = newIndex;
+    _currentIndex = newIndex;
     update();
   }
 }
