9005451c5a65529bef770c767cf0dc8641e785d1
DevNadimm <nadimchowdhury87@gmail.com>
2024-11-15 11:16:14 +0600

    update

--- lib/controllers/controller_binders.dart
+++ lib/controllers/controller_binders.dart
@@ -1,12 +1,13 @@
 import 'package:get/get.dart';
 import 'package:task_mate/controllers/add_new_task_controller.dart';
 import 'package:task_mate/controllers/bottom_nav_controller.dart';
-import 'package:task_mate/controllers/new_task_controller.dart';
+import 'package:task_mate/controllers/new_task_list_controller.dart';
 import 'package:task_mate/controllers/pin_verification_controller.dart';
 import 'package:task_mate/controllers/recover_verify_email_controller.dart';
 import 'package:task_mate/controllers/set_password_controller.dart';
 import 'package:task_mate/controllers/sign_in_controller.dart';
 import 'package:task_mate/controllers/sign_up_controller.dart';
+import 'package:task_mate/controllers/task_status_count_controller.dart';
 import 'package:task_mate/controllers/update_profile_controller.dart';
 
 class ControllerBinders extends Bindings {
@@ -20,6 +21,7 @@ class ControllerBinders extends Bindings {
     Get.put(BottomNavController());
     Get.put(UpdateProfileController());
     Get.put(AddNewTaskController());
-    Get.put(NewTaskController());
+    Get.put(NewTaskListController());
+    Get.put(TaskStatusCountController());
   }
 }
